9f39c15b5f633654c2ae057fb5b235e3994798ef
Simon Ashbery <si.ashbery@gmail.com>
2017-05-12 12:42:14 +0100

    Refactored tests to correctly use doubles for bike and stubs for it's methods

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -6,6 +6,7 @@ describe DockingStation do
       # bike = Bike.new
       bike = double(:bike)
       subject.dock_bike(bike)
+      allow(bike).to receive(:working?).and_return (true)
       expect(subject.release_bike).to eq bike
     end
     it 'raises an error when there are no bikes available' do
@@ -20,10 +21,12 @@ end
     end
   end
 
+  let(:bike) {double(:bike)}
+
   it "releases working bikes" do
     #bike = subject.release_bike
-    # bike = Bike.new
     bike = double(:bike)
+    allow(bike).to receive(:working?).and_return(true)
     expect(bike).to be_working
   end
 
@@ -50,10 +53,12 @@ end
   end
 
   it 'should not release a broken bike' do
-    # my_bike = Bike.new
+    # bike = Bike.new
     bike = double(:bike)
-    my_bike.report_broken
-    subject.dock_bike(my_bike)
+    allow(bike).to receive(:report_broken).and_return (false)
+    allow(bike).to receive(:working?).and_return (false)
+    bike.report_broken
+    subject.dock_bike(bike)
     expect{subject.release_bike}.to raise_error "Cannot release broken bike"
   end
 end
