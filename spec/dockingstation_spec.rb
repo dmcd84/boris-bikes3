2b6bcc3462ea77a5d51130d6d41f16c9ec5d28f0
Simon Ashbery <si.ashbery@gmail.com>
2017-05-12 12:13:28 +0100

    Replaced new instances of the bike class with doubles

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,7 +3,8 @@ require "dockingstation.rb"
 describe DockingStation do
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
+      # bike = Bike.new
+      bike = double(:bike)
       subject.dock_bike(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -14,14 +15,15 @@ end
 
   describe '#dock_bike' do
     it 'raises an error when attempting to dock a bike in a full station' do
-      subject.capacity.times { subject.dock_bike Bike.new }
-      expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock_bike double(:bike)}
+      expect { subject.dock_bike double(:bike) }.to raise_error 'Docking station full'
     end
   end
 
   it "releases working bikes" do
     #bike = subject.release_bike
-    bike = Bike.new
+    # bike = Bike.new
+    bike = double(:bike)
     expect(bike).to be_working
   end
 
@@ -29,13 +31,15 @@ end
   it { is_expected.to respond_to :bike}
 
   it 'docks something' do
-    bike = Bike.new
+    # bike = Bike.new
+    bike = double(:bike)
     subject.dock_bike(bike)
     expect(subject.bike).to eq @bikes
   end
 
   it 'returns docked bike' do
-    bike = Bike.new
+    # bike = Bike.new
+    bike = double(:bike)
     subject.dock_bike(bike)
     expect(subject.bike).to eq @bikes
   end
@@ -46,9 +50,10 @@ end
   end
 
   it 'should not release a broken bike' do
-    my_bike = Bike.new
+    # my_bike = Bike.new
+    bike = double(:bike)
     my_bike.report_broken
     subject.dock_bike(my_bike)
-    expect{subject.release_bike}.to raise_error "Cannot release broken bike" 
+    expect{subject.release_bike}.to raise_error "Cannot release broken bike"
   end
 end
